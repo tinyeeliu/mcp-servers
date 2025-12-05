6908b7b0b44c934e19c2026c9cd48175b3c85bfa
peterliu <peter.liu@aigens.com>
2025-12-05 19:48:10 +0800

    w

--- modules/core/src/main/java/io/mcp/core/utility/Utility.java
+++ modules/core/src/main/java/io/mcp/core/utility/Utility.java
@@ -1,7 +1,15 @@
 package io.mcp.core.utility;
 
 public class Utility {
- 
+    
+
+    private static boolean DEBUG = true;
+
+    public static void setDebug(boolean debug) {
+        DEBUG = debug;
+    }
+
+
     public static void debug(Object... messages) {
        
     }
