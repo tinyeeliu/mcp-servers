7eafaa47902733938eca9460064e1c96d3669994
peterliu <peter.liu@aigens.com>
2025-12-07 21:49:51 +0800

    w

--- deploy/deploy_gae.sh
+++ deploy/deploy_gae.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# Step 1. Build the native image of mcp project
+
+./scripts/build_native.sh
+
+# Step 2. Make a staging directory in target
+# Copy the image to the staging directory
+# Copy the profile app.yaml to the staging directory
+
+IMPLEMENT ME
+
+
+# Step 3. Deploy with gcloud
+
+# Example gcloud deploy command
+# gcloud app deploy ${CI_PROJECT_DIR}/appengine-staging/app.yaml --quiet --project $PROJECT_ID --version $CI_COMMIT_TAG $flags
+
+IMPLEMENT ME
