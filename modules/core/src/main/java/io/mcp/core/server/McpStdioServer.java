a8da3722fde0bb8cbd4a14ba58c03fd5646be453
peterliu <peter.liu@aigens.com>
2025-12-07 17:57:24 +0800

    w

--- modules/core/src/main/java/io/mcp/core/server/McpStdioServer.java
+++ modules/core/src/main/java/io/mcp/core/server/McpStdioServer.java
@@ -1,5 +1,11 @@
 package io.mcp.core.server;
 
+/*
+
+A custom version of STDIO server. This replace the official SDK server because it
+can't be run in native image.
+
+*/
 public class McpStdioServer {
     
 }
